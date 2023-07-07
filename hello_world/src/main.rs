fa0f4013012c433cd8ab52d05ee76af2ab6c2e57
aki <aki@mymail.com>
2023-07-07 15:50:04 +0900

    コンテナBoxを使ってwindowにボタンを追加

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,16 +1,24 @@
 extern crate gtk;
 use gtk::prelude::*;
+use std::error::Error;
 
-fn main() {
-    gtk::init().expect("Failled to initialize GTK.");
+fn main() -> Result<(), Box<dyn Error>> {
+    gtk::init()?;
 
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
     window.set_title("Hello World");
 
-    let label = gtk::Label::new(Some("Hello, World"));
-    window.add(&label);
+    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
 
+    let label = gtk::Label::new(Some("Hello, World"));
+    hbox.pack_start(&label, true, true, 5);
+
+    let button = gtk::Button::with_label("Ok");
+    hbox.pack_start(&button, false, false, 5);
+
+    window.add(&hbox);
     window.show_all();
 
     gtk::main();
+    Ok(())
 }
