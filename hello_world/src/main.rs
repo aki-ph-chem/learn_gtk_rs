4160eaccbfc789c55e8382a32ac276d52a0e82cc
aki <aki@mymail.com>
2023-07-07 23:56:28 +0900

    fix

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,16 +1,31 @@
 extern crate gtk;
 use gtk::prelude::*;
+use std::error::Error;
 
-fn main() {
-    gtk::init().expect("Failled to initialize GTK.");
+fn main() -> Result<(), Box<dyn Error>> {
+    gtk::init()?;
 
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
     window.set_title("Hello World");
 
-    let label = gtk::Label::new(Some("Hello, World"));
-    window.add(&label);
+    let vbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
 
+    let label = gtk::Label::new(Some("Hello, World"));
+    vbox.pack_start(&label, false, false, 5);
+
+    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+
+    let entry = gtk::Entry::new();
+    hbox.pack_start(&entry, false, false, 5);
+
+    let button = gtk::Button::with_label("Ok");
+    hbox.pack_start(&button, false, false, 5);
+
+    vbox.pack_start(&hbox, true, true, 5);
+
+    window.add(&vbox);
     window.show_all();
 
     gtk::main();
+    Ok(())
 }
