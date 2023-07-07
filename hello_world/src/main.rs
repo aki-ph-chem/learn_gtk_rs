dfc6b7336c2c4e529ece15b7e65591a580f23ad7
aki <aki@mymail.com>
2023-07-07 15:58:28 +0900

    コンテナをネストする

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -8,15 +8,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
     window.set_title("Hello World");
 
-    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+    let vbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
 
     let label = gtk::Label::new(Some("Hello, World"));
-    hbox.pack_start(&label, true, true, 5);
+    vbox.pack_start(&label, false, false, 5);
+
+    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+
+    let entry = gtk::Entry::new();
+    hbox.pack_start(&entry, false, false, 5);
 
     let button = gtk::Button::with_label("Ok");
     hbox.pack_start(&button, false, false, 5);
 
-    window.add(&hbox);
+    vbox.pack_start(&hbox, true, true, 5);
+
+    window.add(&vbox);
     window.show_all();
 
     gtk::main();
