105bb51465f3bfc3d798c72abe98e10714edaf74
aki <aki@mymail.com>
2023-07-07 17:34:26 +0900

    Entryから文字列を受け取る

--- signal/src/main.rs
+++ signal/src/main.rs
@@ -12,6 +12,19 @@ fn main() -> Result<(), Box<dyn Error>> {
         Inhibit(false)
     });
 
+    let hbox = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+
+    let entry = gtk::Entry::new();
+    hbox.pack_start(&entry, true, true, 5);
+    
+    let button = gtk::Button::with_label("Say");
+    button.connect_clicked(move |_| {
+        println!("Text: {}", entry.text());
+    });
+
+    hbox.pack_start(&button, false, false, 5);
+
+    window.add(&hbox);
     window.show_all();
     gtk::main();
 
