d8b0a493ba753c2847f8c2e9b7cb96ad549f7cae
aki <aki@mymail.com>
2023-07-07 15:23:51 +0900

    ラベルウィジェットを追加

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -5,8 +5,11 @@ fn main() {
     gtk::init().expect("Failled to initialize GTK.");
 
     let window = gtk::Window::new(gtk::WindowType::Toplevel);
-
     window.set_title("Hello World");
+
+    let label = gtk::Label::new(Some("Hello, World"));
+    window.add(&label);
+
     window.show_all();
 
     gtk::main();
