dd63ff5acaf5fcc12af401fdd5b5b7d8042d6e46
aki <aki@mymail.com>
2023-07-09 23:13:18 +0900

    ただwindowを表示するだけ

---
+++ wiki_books/src/gtk_1.rs
@@ -0,0 +1,21 @@
+// ただwindowを表示するだけ
+extern crate gtk;
+use gtk::prelude::*;
+use std::error::Error;
+
+fn main() -> Result<(),Box<dyn Error>> {
+    gtk::init()?;
+
+    let ui = include_str!("ui/gtk_1/gtk_1.ui");
+    let builder = gtk::Builder::from_string(ui);
+    let window: gtk::Window = builder.object("window_1").expect("Error: window_1");
+    window.connect_delete_event(move |_,_| {
+        gtk::main_quit();
+        Inhibit(false)
+    });
+
+    window.show_all();
+    gtk::main();
+
+    Ok(())
+}
