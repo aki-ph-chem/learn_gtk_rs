28d00ed90eccf8921c157d53d0be079f6a2f907d
aki <aki@mymail.com>
2023-07-10 04:32:12 +0900

    メニューバーのFile/Quit, Aboutのイベント処理まで実装した

--- wiki_books/src/gtk_3.rs
+++ wiki_books/src/gtk_3.rs
@@ -9,34 +9,35 @@ fn main() -> Result<(),Box<dyn Error>> {
     let ui = include_str!("ui/gtk_3.ui");
     let builder = gtk::Builder::from_string(ui);
     let window: gtk::Window = builder.object("window_1").expect("Error: window_1");
-    window.connect_delete_event(move |_,_| {
+    let window_ = window.clone();
+    window_.connect_delete_event(move |_,_| {
         gtk::main_quit();
         Inhibit(false)
     });
 
+    // File/Quitをクリックでプログラムを終了
+    let quit: gtk::MenuItem = builder.object("quit")
+        .expect("Error: quit");
+    quit.connect_activate(move |_|{
+        gtk::main_quit();
+    });
+
+
+    // AbouからAboutDialogが生成される
     let about: gtk::MenuItem = builder.object("about")
         .expect("Error: about"); 
-    let dialog: gtk::MessageDialog = builder.object("dialog")
-        .expect("Error: dialog");
+    let about_dialog: gtk::AboutDialog = builder.object("about_dialog")
+        .expect("Error: about_dialog");
 
-    about.connect_popup_menu(move |_| {
-        gtk::main_quit();
-        false
+    about.connect_activate(move |_|{
+        about_dialog.set_title("About");
+
+        about_dialog.set_authors(&["Aki"]);
+        about_dialog.set_transient_for(Some(&window));
+        about_dialog.show_all();
     });
 
-
-    /*
-    about.connect_clicked(move |_| {
-        let message = "GTK+"
-        dialog.set_text(Some(message));
-        dialog.run();
-        unsafe {
-            dialog.destroy();
-        }
-    });
-    */
-
-    window.show_all();
+    window_.show_all();
     gtk::main();
 
     Ok(())
