0f43c35593432a298a68e11aa4c9b11ec963701c
aki <aki@mymail.com>
2023-07-11 21:33:17 +0900

    button 6, button 7をクリックした時のイベント処理を実装した

--- offcial_examles/src/grid.rs
+++ offcial_examles/src/grid.rs
@@ -2,7 +2,6 @@ extern crate gtk;
 use gtk::prelude::*;
 
 // ToDo
-// 1. button_6, button_7が押されたときのイベント処理
 // 2. Ctrl + q もしくは qが押されたときにプログラムを終了するようにする
 
 fn build_ui(app: &gtk::Application) {
@@ -12,6 +11,38 @@ fn build_ui(app: &gtk::Application) {
     let window: gtk::ApplicationWindow = builder.object("window")
         .expect("Error: windwow");
     window.set_application(Some(app));
+
+    let grid: gtk::Grid = builder.object("grid_1")
+        .expect("Error: grid_1");
+
+    // button 6をクリックしてボタンサイズを変更
+    let button_6: gtk::Button = builder.object("button_6")
+        .expect("Error: button_6");
+    let grid_button_6 = grid.clone();
+    button_6.connect_clicked(move |button| {
+        let height = grid_button_6.cell_height(button);
+        let height_new = if height == 2 {
+            1
+        } else {
+            2
+        };
+        grid_button_6.set_cell_height(button, height_new);
+    });
+
+
+    // button 7をクリックしてボタンの位置を変更
+    let button_7: gtk::Button = builder.object("button_7")
+        .expect("Error: button_6");
+    let grid_button_7 = grid.clone();
+    button_7.connect_clicked(move |button| {
+        let left_attach = grid_button_7.cell_left_attach(button);
+        let left_attach_new = if left_attach == 2 {
+            0
+        } else {
+            left_attach + 1
+        };
+        grid_button_7.set_cell_left_attach(button, left_attach_new);
+    });
 
     window.show_all();
 }
