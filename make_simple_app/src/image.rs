6bb58f7dc835a3b67e99478af64953dc750954b7
aki <aki@mymail.com>
2023-07-15 17:47:32 +0900

    File/Open, Ctrl + oでファイルダイアログを表示

--- make_simple_app/src/image.rs
+++ make_simple_app/src/image.rs
@@ -11,12 +11,46 @@ fn build_ui(app: &gtk::Application) {
         .expect("Error: window");
     window.set_application(Some(app));
 
+    // ショートカットキー
+    let accel_group = gtk::AccelGroup::new(); 
+    window.add_accel_group(&accel_group);
+
     // button: Clickで終了
     let button: gtk::Button = builder.object("button")
         .expect("Error: button");
     button.connect_clicked( glib::clone!(@weak window => move |_| {
         window.close();
     }));
+
+    // File/Quit: クリックで終了
+    let file_quit: gtk::MenuItem = builder.object("file_quit")
+        .expect("Error: file_quit");
+    file_quit.connect_activate(glib::clone!(@weak window => move |_| {
+        window.close();
+    }));
+    // Ctrl + qで終了
+    let (key, modifier) = gtk::accelerator_parse("<Primary>Q");
+    file_quit.add_accelerator("activate", &accel_group, 
+                              key, modifier, gtk::AccelFlags::VISIBLE);
+
+    // File/Open
+    let file_chose: gtk::MenuItem = builder.object("file_chose")
+        .expect("Error: open");
+    // file_chose_dialog
+    let file_chose_dialog: gtk::FileChooserDialog = 
+        builder.object("file_chose_dialog").expect("Error: file_chose_dialog");
+
+    // File/Openのクリックでファイル選択ダイアログを表示
+    file_chose.connect_activate(glib::clone!(@weak window, @weak file_chose_dialog => move |_| {
+        file_chose_dialog.set_title("open file");
+        file_chose_dialog.set_transient_for(Some(&window));
+        file_chose_dialog.run();
+        file_chose_dialog.hide();
+    }));
+    // Ctrl + oでファイル選択ダイアログを表示
+    let (key, modifier) = gtk::accelerator_parse("<Primary>O"); 
+    file_chose.add_accelerator("activate", &accel_group,
+                               key, modifier, gtk::AccelFlags::VISIBLE);
 
     // 画像を表示する
     let path_to_image = "pet_cat_sit.png"; // いらすとやにあった猫の絵
